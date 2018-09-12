1369940e1388d5e14234c159189ac1d45362f6aa
Jared Hanson <jaredhanson@gmail.com>
2018-09-12 16:07:02 -0700

    Use mounted option.

--- app/site.js
+++ app/site.js
@@ -3,8 +3,8 @@ exports = module.exports = function() {
   
   
   var site = kerouac();
-  site.page('/sitemap.xml', require('kerouac-sitemap')());
-  site.page('/sitemap-index.xml', require('kerouac-sitemap').index());
+  site.page('/sitemap.xml', require('kerouac-sitemap')({ mounted: true }));
+  site.page('/sitemap-index.xml', require('kerouac-sitemap').index({ mounted: true }));
   return site;
 };
 
